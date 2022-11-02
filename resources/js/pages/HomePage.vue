a9d805dbfb53e18cb1602c234ee4f5267fb61585
ccwebgroup <ccwebgroup@gmail.com>
2022-11-02 22:14:09 +0800

    initial commit

---
+++ resources/js/pages/HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <v-card class="mx-auto">
+            <v-img
+                class="align-end text-white"
+                height="200"
+                src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+                cover
+            >
+                <v-card-title>Top 10 Australian beaches</v-card-title>
+            </v-img>
+        </v-card>
+    </div>
+</template>
+
+<script setup></script>
